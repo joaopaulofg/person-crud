da3a7f46d18aab386dc172acd194ebed8dd0de92
joaopaulofg <joaofpaulo1@gmail.com>
2026-07-10 11:39:48 -0300

    feat: configure request log messaging

--- src/main/java/com/joaopaulofg/personcrud/log/config/RabbitMQConfig.java
+++ src/main/java/com/joaopaulofg/personcrud/log/config/RabbitMQConfig.java
@@ -1,4 +1,67 @@
 package com.joaopaulofg.personcrud.log.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(RequestLogMessagingProperties.class)
 public class RabbitMQConfig {
+
+    @Bean
+    public DirectExchange requestLogExchange(RequestLogMessagingProperties properties) {
+        return new DirectExchange(properties.requestLogExchange(), true, false);
+    }
+
+    @Bean
+    public Queue requestLogQueue(RequestLogMessagingProperties properties) {
+        return new Queue(properties.requestLogQueue(), true);
+    }
+
+    @Bean
+    public Binding requestLogBinding(
+            Queue requestLogQueue,
+            DirectExchange requestLogExchange,
+            RequestLogMessagingProperties properties
+    ) {
+        return BindingBuilder
+                .bind(requestLogQueue)
+                .to(requestLogExchange)
+                .with(properties.requestLogRoutingKey());
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory,
+            MessageConverter jsonMessageConverter
+    ) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            MessageConverter jsonMessageConverter
+    ) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter);
+        return factory;
+    }
 }
